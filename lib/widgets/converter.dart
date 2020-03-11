f1cceeacc8a1b90b017fe380336560252b2b6c21
Cristian David Zapata Lazaro <lazarod@CAMPUSUNINORTE.LOCAL>
2020-03-10 20:17:26 -0500

    nada

---
+++ lib/widgets/converter.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class Converter extends StatefulWidget {
+  @override
+  _ConverterState createState() => _ConverterState();
+}
+
+class _ConverterState extends State<Converter> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
