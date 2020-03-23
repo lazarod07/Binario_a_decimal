8297192d290d995168303bbf1b47b7da5e549a58
lazarod07 <lazarod@uninorte.edu.co>
2020-03-22 23:53:14 -0500

    Boton clear

--- lib/widgets/converter.dart
+++ lib/widgets/converter.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 
@@ -9,8 +10,64 @@ class Converter extends StatefulWidget {
 class _ConverterState extends State<Converter> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("Decimal to binary"),
+      ),
+      body: new Center(
+        child: Container(
+          margin: const
+          EdgeInsets.all(10.0),
+          alignment: 
+          Alignment.center,
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(10.0),
+                  color: Colors.deepOrange,
+                  alignment: Alignment.center,
+                  child: new Text("First row",
+                    style: new TextStyle(fontSize: 20.0)
+                  ),
+                )
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(10.0),
+                  color: Colors.deepOrange,
+                  alignment: Alignment.center,
+                  child: new Text("First row",
+                    style: new TextStyle(fontSize: 20.0)
+                  ),
+                )
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(10.0),
+                  alignment: Alignment.center,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    textColor: Colors.white,
+                    child: const Text(
+                      'Clear',
+                      style: TextStyle(fontSize: 20)
+                    ),
+                    color: Colors.blue,
+                  ),
+                ),
+              )
+            ],
+          )
+        )
+      ),
     );
   }
 }
